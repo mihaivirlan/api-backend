5b34a04611d10e2b3d63f1410890ed98fcac8124
Mihai <mihaivirlan1@mail.ru>
2018-10-07 19:07:13 +0300

    Added mysql support

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var utils = require("./utils.js");
+var db = require("./env.js")
 
 var app = express();
 
-// app.get("/url", (req, res, next) => {
-//     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-// });
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+ host     : db.mysql.host,
+ user     : db.mysql.user,
+ password : db.mysql.password,
+ database : db.mysql.database
+});
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+ console.log('The solution is: ', rows[0].solution);
+ //console.log(err);
+})
+
+connection.end()
 
 app.route("/data")
     .get(utils.getFunction)
@@ -13,9 +29,6 @@ app.route("/data")
     .patch(utils.patchFunction)
     .put(utils.putFunction)
     .delete(utils.deleteFunction);
-
-
-
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
--- utils.js
+++ utils.js
@@ -6,6 +6,22 @@ exports.getFunction = (req, res) => {
 
 exports.postFunction = (req, res) => {
     res.json("this is a post request")
+        // .post((req, res) => {
+        //     let book = new Book({title: 'The Bull', author: 'Saki'});
+        //     book.save();
+        //     res.status(201).send(book)
+        // })
+
+    // exports.post("/data", (req, res) => {
+    //     var myData = new User(req.body);
+    //     myData.save()
+    //         .then(item => {
+    //             res.send("item saved to database");
+    //         })
+    //         .catch(err => {
+    //             res.status(400).send("unable to save to database");
+    //         });
+    // });
 };
 
 exports.putFunction = (req, res) => {
